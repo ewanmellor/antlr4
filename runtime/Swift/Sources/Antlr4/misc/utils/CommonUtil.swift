ecaf77ad21a364e2d77b4db9bebf0f6da0a2054e
Ewan Mellor <commits@ewanmellor.org>
2019-08-28 16:15:22 -0700

    [Swift] Tidy-up around ATNDeserializer.
    Make the ATNDeserializer.*factory methods static and private.
    
    Remove ATNSimulator.edgeFactory.  This is deprecated in the Java runtime,
    so we didn't need it in this port in the first place.
    
    Don't force-unwrap the return value from stateFactory.  Returning nil
    is meaningful here (it means that the given type was INVALID_TYPE).
    The Java implementation handles this and proceeds to add the null state
    to the ATN; we should do the same.
    
    Move some helper functions from CommonUtil into ATNDeserializer, since
    they are not common at all.

--- runtime/Swift/Sources/Antlr4/misc/utils/CommonUtil.swift
+++ runtime/Swift/Sources/Antlr4/misc/utils/CommonUtil.swift
@@ -15,23 +15,3 @@ import Foundation
 func errPrint(_ msg: String) {
     fputs(msg + "\n", stderr)
 }
-
-func toInt(_ c: Character) -> Int {
-    return c.unicodeValue
-}
-
-func toInt32(_ data: [Character], _ offset: Int) -> Int {
-    return data[offset].unicodeValue | (data[offset + 1].unicodeValue << 16)
-}
-
-func toLong(_ data: [Character], _ offset: Int) -> Int64 {
-    let mask: Int64 = 0x00000000FFFFFFFF
-    let lowOrder: Int64 = Int64(toInt32(data, offset)) & mask
-    return lowOrder | Int64(toInt32(data, offset + 2) << 32)
-}
-
-func toUUID(_ data: [Character], _ offset: Int) -> UUID {
-    let leastSigBits: Int64 = toLong(data, offset)
-    let mostSigBits: Int64 = toLong(data, offset + 4)
-    return UUID(mostSigBits: mostSigBits, leastSigBits: leastSigBits)
-}
