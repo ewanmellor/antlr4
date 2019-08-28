265b8b30de763b3c40d9e27aec574c95a16c9f0f
Ewan Mellor <commits@ewanmellor.org>
2019-08-28 16:16:43 -0700

    [Swift] Various fixes for BitSet.
    Change the internals of BitSet to use UInt64, not Int64.  The original
    implementation was apparently taken from OpenJDK's BitSet, but that uses
    signed integers only because Java doesn't have unsigned integers.  Swift
    does.  By using unsigned integers, we don't need our own
    >>> operator (unsigned-right-bit-shift), because Swift's built-in >>
    operator does the right thing when it is operating on unsigned integers.
    
    Fix {previous,next}{Clear,Set}Bit and the toIndex versions of set, clear,
    get, and flip.  The OpenJDK implementation uses
    WORD_MASK = 0xffffffffffffffffL, but we were using WORD_MASK = Int64.max.
    This is missing the top bit, so any operation using it was broken.
    
    Note also that in Java (n >>> -1) is equivalent to (n >>> (-1 & 63)), but
    in Swift the equivalent is not true.  OpenJDK's implementation relies on
    this as a trick when calculating lastWordMask.  We were hiding this in
    our >>> implementation, with a "% 64" thrown in there.  This is now explicit
    in the lastWordMask initializations instead.
    
    Remove the sizeIsSticky field.  This was written but never read.  It comes
    from an optimization in the OpenJDK implementation, and is not used here.
    
    Remove the serialVersionUID field.  This is only meaningful in Java.
    
    This also adds some unit tests for BitSet.

--- runtime/Swift/Sources/Antlr4/misc/utils/CommonUtil.swift
+++ runtime/Swift/Sources/Antlr4/misc/utils/CommonUtil.swift
@@ -16,31 +16,6 @@ func errPrint(_ msg: String) {
     fputs(msg + "\n", stderr)
 }
 
-infix operator >>> : BitwiseShiftPrecedence
-
-func >>>(lhs: Int32, rhs: Int32) -> Int32 {
-    let left = UInt32(bitPattern: lhs)
-    let right = UInt32(bitPattern: rhs) % 32
-
-    return Int32(bitPattern: left >> right)
-}
-
-func >>>(lhs: Int64, rhs: Int64) -> Int64 {
-    let left = UInt64(bitPattern: lhs)
-    let right = UInt64(bitPattern: rhs) % 64
-
-    return Int64(bitPattern: left >> right)
-}
-
-func >>>(lhs: Int, rhs: Int) -> Int {
-    let numberOfBits: UInt = MemoryLayout<UInt>.size == MemoryLayout<UInt64>.size ? 64 : 32
-
-    let left = UInt(bitPattern: lhs)
-    let right = UInt(bitPattern: rhs) % numberOfBits
-
-    return Int(bitPattern: left >> right)
-}
-
 func toInt(_ c: Character) -> Int {
     return c.unicodeValue
 }
